d703eb3f9442f09746caccc3006f9ac4d5005cd4
mahmudgithub <mahmudhossain838@gmail.com>
2021-06-12 20:17:03 +0600

    again start

---
+++ c/ArrayDataStructure.c
@@ -0,0 +1,22 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[2];
+//     arr[0]=10;
+//     arr[1]=20;
+
+//     printf(arr[0]);
+//     return 0;
+// }
+
+
+#include <stdio.h>
+
+ int main()
+ {
+    int arr[0];
+    printf("input int value:");
+    scanf("%d", &arr[0]);
+    printf(arr[0]);
+    return 0;
+ }
