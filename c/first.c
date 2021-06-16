94efdb7725ddf45dc6a325a3121eea446dd75b77
mahmudgithub <mahmudhossain838@gmail.com>
2021-06-16 16:06:35 +0600

    show char value

--- c/first.c
+++ c/first.c
@@ -8,12 +8,21 @@
 // }
 
 
-// show inter value
+// // show inter value
+// #include <stdio.h>
+// int main()
+// {
+//     int roll=12345;
+//     printf("my roll is : %d ", roll);
+//     return 0;
+// }
 
-#include <stdio.h>
-int main()
-{
-    int roll=12345;
-    printf("my roll is : %d ", roll);
-    return 0;
-}
+// // show character value
+// #include<stdio.h>
+// int main()
+// {
+//     char name='b';
+//     printf("my name is : %c", name);
+//     return 0;
+
+// }
