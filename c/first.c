ce5afc6310465ab056f149aa8f70f30bc02ed957
mahmudgithub <mahmudhossain838@gmail.com>
2021-06-16 16:00:53 +0600

    first value show

--- c/first.c
+++ c/first.c
@@ -1,7 +1,19 @@
-#include<stdio.h>
+// #include<stdio.h> 
 
+// int main()
+
+// {
+//     printf("i am mahmud");
+//     return 0;
+// }
+
+
+// show inter value
+
+#include <stdio.h>
 int main()
 {
-    printf("hello world");
+    int roll=12345;
+    printf("my roll is : %d ", roll);
     return 0;
 }
