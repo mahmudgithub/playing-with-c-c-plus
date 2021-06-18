fbea57b708b6d1f13bac6502781670d1d8605d8d
mahmudgithub <mahmudhossain838@gmail.com>
2021-06-19 02:01:03 +0600

    add all files

---
+++ python.py
@@ -0,0 +1,16 @@
+# def fn(x,y):
+#     sum=x+y
+#     print("result is :",sum)
+# fn(20,2)
+
+
+# def fn(x,y):
+#     print("result is :",x+y)
+# a=int(input("input value of x:"))
+# b=int(input("input value of y"))
+# fn(a,b)
+
+
+# array in python 
+arr=['mahmud','hossain','nannu']
+print(arr[1])
---
+++ script.js
@@ -0,0 +1,6 @@
+// function fn(x,y,sum)
+// {
+// sum=x+y;
+// console.log(sum);
+// }
+// fn(22,2)
