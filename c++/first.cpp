1d57fe6ad142c1ef2aaf73717f652269f159e84e
mahmudgithub <mahmudhossain838@gmail.com>
2021-07-06 18:23:53 +0600

    first program of c++ from w3 school

--- c++/first.cpp
+++ c++/first.cpp
@@ -1,8 +1,225 @@
-// Your First C++ Program
+// // Your First C++ Program
 
-#include <iostream>
+// #include <iostream>
 
-int main() {
-    std::cout << "Hello World i am mahmud!";
+// int main() {
+//     std::cout << "Hello World i am mahmud!";
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"hello world";
+//     return 0;
+// } 
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"hello " <<"mahmud";
+//     return 0;
+ 
+// }
+
+
+
+// // use different  type of black slash
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y;
+//     x=10;
+//     y=10;
+//     int z=x+y;
+//     cout<<"result is : \n" <<z <<endl;  //use new line 
+//     cout<<"result is : "<<endl <<z<<endl; //endl is same as new line
+//     return 0;
+
+// }
+
+    
+// // string in c++
+// #include<iostream>>
+// using namespace std;
+// int main()
+// {
+//     char name[20]="mahmud hossain";
+//     cout<<"my name is :"<<name;
+// } // namespace std;
+
+// // user input in c++
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"enter a intiger value:";
+//     cin>>x;
+//     cout<<"result value:"<<x;
+// } 
+
+
+// // take diffrent type of user data without mentioning data type 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     float y;
+//     char name[20];
+//     double z;
+//     cout<<"enter x value:";cin>>x;cout<<"RESULT IS :"<<x <<endl;
+//     cout<<"enter y value :";cin>>y;cout<<"result is :"<<y<<endl;
+//     cout<<"enter name:";cin>>name;cout <<"result is :"<<name<<endl;
+//     cout<<"enter z value:";cin>>z;cout<<"rsult is :"<<z<<endl; 
+// } 
+
+
+
+// // take multiple user input at a line in c++
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y,z;
+//     cout <<"enter 3 int values:";
+//     cin>>x>>y>>z;
+//     int sum;
+//     sum=x+y+z;
+//     cout <<"result :"<<sum;
+// } 
+
+
+
+// // type casting in c++
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y;
+//     cout<<"enter two number:";
+//     cin>>x>>y;
+//     int sum =x+y;
+//     cout<<"result:"<<sum;
+// }
+
+
+// // relational operetor in c++
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char vow;
+//     cout<<"enter char value:";
+//     cin>>vow;
+//     if(vow=='a' || vow=='e' || vow=='i' || vow=='i' || vow=='o' || vow=='u')
+//     cout<<"enter char in vowel:"<<vow;
+//     else
+//     cout<<"enter char value is consonenet:"<<vow;  
+// } 
+
+
+// // relational and operetor in c++
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y,z;
+//     cout<<"enter 3 number:";
+//     cin>>x>>y>>z;
+//     if (x<y && x<z) cout<<"x is smaller then all";
+//     else if(x>y && x>z) cout<<"x is bigger then all";
+//     else 
+//     cout<<"unknown input ";    
+// } 
+
+
+// // switch statement in c++ 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num;
+//     cout<<"enter a number:";
+//     cin>>num;
+//     switch (num)
+//     {
+//     case 0:
+//         cout<<"enter numbr is:"<<num;
+//         break;
+//     case 1:
+//         cout<<"enter numbr is:"<<num;
+//         break;
+//     case 2:
+//         cout<<"enter numbr is:"<<num;
+//         break;
+//     case 3:
+//         cout<<"enter numbr is:"<<num;
+//         break;
+    
+//     default:
+//     cout<<"input is out of range";
+//         break;
+//     }
+// } 
+
+
+
+// // define class in c++
+// #include<iostream>
+// using namespace std;
+// class one
+// {
+//     public:
+//     int x,y,sum;
+//     sum=x+y;
+
+// };
+
+// int main()
+// {
+//     cout<<"enter two values:";
+    
+ 
+// } 
+
+
+// #include <iostream>
+// using namespace std;
+
+// class Student
+// {
+//     public:
+//         int roll;
+//         int name;
+        
+// };
+
+// int main()
+
+// {
+    
+//     Student su;
+//     su.roll=1234;
+//     su.name=5;
+//     cout<< su.name<<endl;
+//     cout<<su.roll;
+
+// } 
+
+
+// c++ from w3 school .........................................
+// run first c++ program 
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout <<"'hello world'";
     return 0;
-}
+    
+} 
